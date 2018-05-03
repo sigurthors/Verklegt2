1df95b5f4b1f930f2f5d739725846520f52afc03
aSnorri <adalberg17@ru.is>
2018-05-03 15:50:05 +0000

    i
    +Isbn, categoryId, coverImg

--- BookCave/Data/EntityModels/Book.cs
+++ BookCave/Data/EntityModels/Book.cs
@@ -8,5 +8,8 @@ namespace BookCave.Data.EntityModels
         public int ReleaseYear { get; set; }
         public double Rating { get; set; }
         public int AuthorId { get; set; }
+        public string Isbn { get; set; }
+        public int CategoryId { get; set; }
+        public string CoverImg { get; set; }
     }
 }
