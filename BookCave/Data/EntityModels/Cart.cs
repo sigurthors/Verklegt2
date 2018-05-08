34a592c712b0f468e624001c21e29e7f232da71c
Sigurþór Sumarliðason <sigurthor@Sigurthors-MacBook-Pro.local>
2018-05-08 13:52:47 +0000

    Updated Cart - Signed in user can only add to cart

--- BookCave/Data/EntityModels/Cart.cs
+++ BookCave/Data/EntityModels/Cart.cs
@@ -5,10 +5,8 @@ namespace BookCave.Data.EntityModels
 {
     public class Cart
     {
-        public int CartId { get; set; }
-        public int Quantity { get; set; }
-        public List<Book> Books { get; set; }
+        public int Id { get; set; }
         public int UserId { get; set; }
-        public DateTime DateCreated { get; set; }
+        public int BookId { get; set; }
     }
 }
---
+++ BookCave/Migrations/20180508130635_UpdateCart.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookCave.Migrations
+{
+    public partial class UpdateCart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Carts_CartId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_CartId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "Quantity",
+                table: "Carts",
+                newName: "BookId");
+
+            migrationBuilder.RenameColumn(
+                name: "CartId",
+                table: "Carts",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BookId",
+                table: "Carts",
+                newName: "Quantity");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Carts",
+                newName: "CartId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Carts",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "CartId",
+                table: "Books",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_CartId",
+                table: "Books",
+                column: "CartId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Carts_CartId",
+                table: "Books",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "CartId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
