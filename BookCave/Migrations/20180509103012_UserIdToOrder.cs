4f14ac001bbabf1b620b12641ce5b10336990ac3
Sigurþór Sumarliðason <sigurthor@Sigurthors-MacBook-Pro.local>
2018-05-09 10:30:49 +0000

    Order Migration

---
+++ BookCave/Migrations/20180509103012_UserIdToOrder.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookCave.Migrations
+{
+    public partial class UserIdToOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OrderId",
+                table: "Orders",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Orders",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Orders",
+                newName: "OrderId");
+        }
+    }
+}
