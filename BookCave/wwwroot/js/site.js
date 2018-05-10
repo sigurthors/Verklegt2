665aee65b0073f73259fde6b0b8f187c26c07e8b
aSnorri <adalberg17@ru.is>
2018-05-10 11:01:37 +0000

    Carousel finished
    s

--- BookCave/wwwroot/js/site.js
+++ BookCave/wwwroot/js/site.js
@@ -31,3 +31,18 @@ $( document ).ready(function() {
 $('.carousel').carousel({
     interval: 3000
   })
+
+  $('.carousel .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    if (next.next().length>0) {
+      next.next().children(':first-child').clone().appendTo($(this));
+    }
+    else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+  });
