e0feee89d87fd078351ba819a778960b159f127c
aSnorri <adalberg17@ru.is>
2018-05-10 15:08:40 +0000

    Author details css
    s

--- BookCave/Models/ViewModels/BookInAuthorModel.cs
+++ BookCave/Models/ViewModels/BookInAuthorModel.cs
@@ -8,5 +8,7 @@ namespace BookCave.Models.ViewModels
         public int AuthorId { get; set; }
         public string Author { get; set; }
         public float Price { get; set; }
+        public int ReleaseYear { get; set; }
+        public double Rating { get; set; }
     }
 }
