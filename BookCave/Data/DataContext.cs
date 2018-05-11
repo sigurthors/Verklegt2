253941089419f41a86780f07aba05af9d05a4934
egillthor96 <egillj17@ru.is>
2018-05-11 11:41:38 +0000

    commit

--- BookCave/Data/DataContext.cs
+++ BookCave/Data/DataContext.cs
@@ -8,7 +8,6 @@ namespace BookCave.Data
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<User> Users { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<Comment> Comments { get; set; }
